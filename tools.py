e9cd58bb0857fdfa11631a507f3086dfb9bb8e56
ZkariaGamalAloha <zkaria.gamal@alohadot.net>
2025-12-14 13:02:05 +0200

    handle file system search

--- tools.py
+++ tools.py
@@ -1,6 +1,4 @@
 from tools_module import (
-    findFile,
-    readFile,
     runDeployScript,
     openVsCode,
     killProcess,
@@ -8,17 +6,23 @@ from tools_module import (
     network_tools,
 )
 from tools_module.dangerous_tools import emptyTrash, emptyTmp
-
+from tools_module.files_tools import findFile, readFile , openFile
 __all__ = [
     findFile.findfile,
     readFile.read_file,
+    openFile.open_file,
+
     runDeployScript.run_deploy_script,
+
     openVsCode.open_vscode,
+    openBrowser.open_browser,
+
     emptyTrash.empty_trash,
     emptyTrash.remove_file,
     emptyTmp.clear_tmp,
+    
     killProcess.kill_process,
-    openBrowser.open_browser,
+
     network_tools.check_internet,
     network_tools.enable_wifi,
 ]
