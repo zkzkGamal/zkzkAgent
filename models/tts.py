e01a1603215783473b0bf552509c47dafa86b20d
zkaria gamal <zekogml11@gmail.com>
2026-06-25 22:20:34 +0300

    Fix agent behavior and plan tools

--- models/tts.py
+++ models/tts.py
@@ -1,20 +1,3 @@
-# from kokoro import KPipeline
-# import sounddevice as sd
-# import torch
-
-# pipeline = KPipeline(lang_code='a')
-
-# def speak(text):
-#     if not text or not text.strip():
-#         text = "error the generate content pls try again"
-#     generator = pipeline(text, voice='af_heart')
-#     for i, (gs, ps, audio) in enumerate(generator):
-#         print(f"[Chunk {i}] gs={gs}, ps={ps}, audio_len={len(audio)}")
-#     # Play audio chunk immediately
-#     sd.play(audio, samplerate=24000)
-#     sd.wait()  # Wait until this chunk finishes before continuing
-
-
 import wave
 import sounddevice as sd
 import soundfile as sf  
